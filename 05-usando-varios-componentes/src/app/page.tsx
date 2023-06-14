95efc12cd8a7ba2b3f903994049d50265e4f5567
Mateus Alves <contatomateusalves@hotmail.com>
2023-06-14 20:45:21 -0300

    Studies React ⚙️

--- 05-usando-varios-componentes/src/app/page.tsx
+++ 05-usando-varios-componentes/src/app/page.tsx
@@ -1,17 +1,26 @@
 import { DuoComp } from "@/components/DuoComp";
+import { Person } from "@/components/Person";
 
 const Page = () => {
+  // Retorno root deve ser um único comp
   return (
-    <div>
-      <h1 className="m-4 font-bold text-2xl">Hello Word</h1>
-      <p className="m-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non perferendis
-        ex, saepe ab ipsa reprehenderit distinctio optio, similique mollitia
-        natus, quidem excepturi dolor molestias aliquam placeat numquam et
-        sapiente provident.
-      </p>
-      <DuoComp />
-    </div>
+    //utilizar "div" única
+    // <div>
+    //   <h1 className="m-4 font-bold text-2xl">Hello Word</h1>
+    //   <p className="m-4">
+    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Non perferendis
+    //     ex, saepe ab ipsa reprehenderit distinctio optio, similique mollitia
+    //     natus, quidem excepturi dolor molestias aliquam placeat numquam et
+    //     sapiente provident.
+    //   </p>
+    //   <DuoComp />
+    // </div>
+
+    // Ou utilizar "Fragment"
+    // renderiza filhos sem 'comp' pai
+    <>
+      <Person />
+    </>
   );
 };
 
---
+++ 05-usando-varios-componentes/src/components/Person.tsx
@@ -0,0 +1,13 @@
+export const Person = () => {
+  return (
+    <>
+      <h1 className="w-52 h-15 bg-white text-black my-4 text-center font-bold">
+        Elon Musk
+      </h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/452px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
+        alt="Foto Elon Musk"
+      />
+    </>
+  );
+};
