5da0e49f82fe05cd49d985db1e560bc77beed211
Mateus Alves <contatomateusalves@hotmail.com>
2023-06-19 19:58:31 -0300

    Studies React ⚙️

--- 05-usando-varios-componentes/src/components/Person.tsx
+++ 05-usando-varios-componentes/src/components/Person.tsx
@@ -1,13 +1,24 @@
+const getDay = () => {
+  return new Intl.DateTimeFormat("pt-BR", { weekday: "long" }).format(
+    new Date()
+  );
+};
+
 export const Person = () => {
+  const data = {
+    name: "Elon Musk",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/452px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+  };
+
+  // console.log(getDay());
+
   return (
     <>
       <h1 className="w-52 h-15 bg-white text-black my-4 text-center font-bold">
-        Elon Musk
+        {data.name}
       </h1>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/452px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
-        alt="Foto Elon Musk"
-      />
+      <img src={data.avatar} alt={data.name} />
     </>
   );
 };
