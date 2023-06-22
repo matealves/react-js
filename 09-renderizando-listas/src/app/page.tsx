cd6971fa271a5ec055bfb364b4304210fb69bcae
Mateus Alves <contatomateusalves@hotmail.com>
2023-06-22 20:10:43 -0300

    Studies React ⚙️

---
+++ 09-renderizando-listas/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { peopleList } from "@/data/peopleList";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Olá Mundo</h1>
+      <h3>Algum outro texto</h3>
+
+      {peopleList.length > 0 && (
+        <ul>
+          {peopleList.map((person) => (
+            <li key={person.id}>
+              • {person.name} - {person.profession}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Page;
---
+++ 09-renderizando-listas/src/data/peopleList.ts
@@ -0,0 +1,8 @@
+import { Person } from "@/types/Person";
+
+export const peopleList: Person[] = [
+  { id: 1, name: "Mateus", profession: "Developer" },
+  { id: 2, name: "Gabriel", profession: "Planned" },
+  { id: 3, name: "Roane", profession: "Analyst" },
+  { id: 4, name: "Cicero", profession: "Assistant" },
+];
