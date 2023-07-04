565fecd04a279ae95b4d76191395601cfe648d42
Mateus Alves <contatomateusalves@hotmail.com>
2023-07-04 19:53:04 -0300

    Studies React ⚙️

---
+++ 12-ex_tabela-notas-escolares/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { StudentTable } from "@/components/StudentTable";
+import { students } from "@/data/students";
+
+const Page = () => {
+  return (
+    <div className="container mx-auto p-20">
+      <StudentTable students={students} />
+    </div>
+  );
+};
+
+export default Page;
