6d6234247a68d5bc00926c79168dde64fd81265b
Mateus Alves <contatomateusalves@hotmail.com>
2023-08-07 19:40:16 -0300

    Studies React ⚙️

--- 19-arrays-em-states/src/app/page.tsx
+++ 19-arrays-em-states/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { TodoItem } from "@/types/TodoItem";
 import { useState } from "react";
 
 const Page = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<TodoItem[]>([
+    { label: "Fazer o almoço", checked: false },
+    { label: "Limpar a cozinha", checked: false },
+  ]);
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
       <h1 className="text-3xl mt-5">Lista de Tarefas</h1>
 
       <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-700">
@@ -18,12 +22,10 @@ const Page = () => {
         <button className=" rounded bg-blue-500 text-sm px-7">Adicionar</button>
       </div>
       <ul className="w-full max-w-lg list-disc pl-5">
-        <li>
-          Tarefa 1 - <button className="hover:underline">[Deletar]</button>
-        </li>
-        <li>
-          Tarefa 2 - <button className="hover:underline">[Deletar]</button>
-        </li>
+        {list.map((item, index) => (
+          <li>{item.label} - <button className="hover:underline">[Deletar]</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
