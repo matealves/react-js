8e9c883e309d5040caf8857d5425c8f29e621550
Mateus Alves <contatomateusalves@hotmail.com>
2023-12-12 21:15:48 -0300

    Using API ⚙️

--- 31-api/src/app/page.tsx
+++ 31-api/src/app/page.tsx
@@ -1,19 +1,60 @@
 "use client";
 
+import { User } from "@/types/User";
 import { useEffect, useState } from "react";
 
 const Page = () => {
-const [users, setUsers] = useState([])
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getUsers = async () => {
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    setUsers(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => console.log("data:", data));
+    getUsers();
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setUsers(data);
+    //   })
+    //   .catch(() => {
+    //     console.error("ERRO");
+    //   })
+    //   .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl text-center mt-6 font-bold">Lista de usuários</h1>
+    <div className="container">
+      <h1 className="text-2xl w-screen block text-center mt-6 font-bold mb-4">
+        Lista de usuários
+      </h1>
+      {loading && (
+        <h1 className="text-1xl w-screen block text-center mt-6 font-bold mb-4">
+          Carregando...
+        </h1>
+      )}
+      {!loading && users.length > 0 && (
+        <ul className="flex w-screen flex-wrap justify-center items-center px-6">
+          {users.map(({ id, name, email, address }) => (
+            <li className="m-3 text-sm w-72 border p-2" key={id}>
+              <p>ID: {id}</p>
+              <p>Nome: {name}</p>
+              <p>E-mail: {email}</p>
+              <p>Cidade: {address.city}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+      {!loading && !users.length && (
+        <h1 className="text-1xl w-screen block text-center mt-6 font-bold mb-4">
+          Não há usuários para exibir...
+        </h1>
+      )}
     </div>
   );
 };
