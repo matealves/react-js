e6497b8c72495b35e333daa967c39cdf863e6120
Mateus Alves <contatomateusalves@hotmail.com>
2024-09-10 20:43:13 -0300

    feat: Fazendo query sob demanda

--- 33-tan_stack_query/src/app/page.tsx
+++ 33-tan_stack_query/src/app/page.tsx
@@ -1,21 +1,50 @@
 "use client";
 
 import { usePost, usePosts } from "@/utils/queries";
+import { useState } from "react";
 
 const Page = () => {
-  const posts = usePosts();
+  const [canLoadPosts, setCanLoadPosts] = useState(false);
+  const posts = usePosts(canLoadPosts);
   // const postItem = usePost(10);
 
+  const handleLoadPostsButton = () => {
+    setCanLoadPosts(true);
+  };
+
   return (
-    <div className="container mx-auto flex flex-col gap-3 items-center">
-      <h1>Posts:</h1>
+    <div className="container mx-auto my-8 flex flex-col gap-3 items-center">
+      <h1 className="text-xl font-bold">Feed</h1>
 
       {posts.isLoading && "Loading..."}
+
+      {!canLoadPosts && (
+        <button
+          className="bg-blue-600 text-sm font-semibold rounded-md p-2 mt-4"
+          onClick={handleLoadPostsButton}
+        >
+          Carregar posts
+        </button>
+      )}
 
       {posts.data && (
         <ul>
           {posts.data.map((item) => (
-            <li key={item.id}>{item.title}</li>
+            <li
+              key={item.id}
+              className="border border-white border-opacity-30 mb-5 py-3 px-5 rounded-md max-w-md"
+            >
+              <div className="flex items-center h-8 mb-3">
+                <div className="bg-white opacity-10 rounded-full w-8 h-8"></div>
+                <div className="text-xs ml-2 opacity-60 font-medium">
+                  @username
+                </div>
+              </div>
+
+              <div className="font-bold text-sm">{item.title}</div>
+
+              <div className="text-xs font-light mt-2">{item.body}</div>
+            </li>
           ))}
         </ul>
       )}
--- 33-tan_stack_query/src/utils/queries.ts
+++ 33-tan_stack_query/src/utils/queries.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPost, getPosts, getUser, getUsers } from "./api";
 
-export const usePosts = () => {
+export const usePosts = (enabled?: boolean) => {
   return useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
     staleTime: 5 * (60 * 1000), // 5 minutos
+    enabled, // executar ao iniciar?
   });
 };
 
@@ -13,7 +14,6 @@ export const usePost = (id: number) => {
   return useQuery({
     queryKey: ["posts", id],
     queryFn: () => getPost(id),
-    staleTime: Infinity,
   });
 };
 
@@ -21,7 +21,6 @@ export const useUsers = () => {
   return useQuery({
     queryKey: ["users"],
     queryFn: getUsers,
-    staleTime: 10 * (60 * 1000),
   });
 };
 
@@ -29,6 +28,5 @@ export const useUser = (id: number) => {
   return useQuery({
     queryKey: ["users", id],
     queryFn: () => getUser(id),
-    staleTime: Infinity,
   });
 };
